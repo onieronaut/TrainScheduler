4ef4720907508a1cbc899956519805002540a4c1
jerard <qbin221@gmail.com>
2019-10-30 17:57:04 -0400

    finished javascript logic, moment.js logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,59 @@ firebase.initializeApp(firebaseConfig);
 
 let db = firebase.database();
 
+//Retrieving and submitting user data to store in Firebase
 $("#submit-button").on("click", function () {
     event.preventDefault();
 
     let name = $("#name").val().trim();
     let destination = $("#destination").val().trim();
     let firstTrain = $("#first-train").val().trim();
-    let freq = $("#frequency").val.trim();
-  })
+    let freq = $("#frequency").val().trim();
+
+    db.ref().push({
+        name: name,
+        destination: destination,
+        freq: freq,
+        firstTrain: firstTrain
+    })
+
+    $("#name").val("");
+    $("#destination").val("");
+    $("#first-train").val("");
+    $("#frequnecy").val("");
+
+});
+
+db.ref().on("child_added", function(snap) {
+
+    name = snap.val().name;
+    destination = snap.val().destination;
+    freq = snap.val().freq;
+    firstTrain = snap.val().firstTrain;
+
+    let firstTrainConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+    let currentTime = moment();
+    let diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+    let tRemainder = diffTime % freq;
+    let nextArrival = freq - tRemainder;
+    let nextTrain = moment().add(nextArrival, "minutes").format("hh:mm A");
+
+
+    let newRow = $("<tr>");
+    let newName = $("<td>").text(name);
+    let newDestination = $("<td>").text(destination);
+    let newFreq = $("<td>").text(freq);
+    let newArrival = $("<td>").text(nextArrival);
+    let newMinAway = $("<td>").text(nextTrain);
+
+    newRow.append(
+        newName,
+        newDestination,
+        newFreq,
+        newMinAway,
+        newArrival
+    );
+
+    $(".new-row").append(newRow);
+
+});
